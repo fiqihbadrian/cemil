2740960feb4013bd29614d7eda5cbd7523d93d91
bdr.de <76670473+fiqihbadrian@users.noreply.github.com>
2025-09-01 15:16:28 +0700

    Update kripik.ts

--- src/app/data/kripik.ts
+++ src/app/data/kripik.ts
@@ -1,11 +1,11 @@
 export const kripikProducts = [
     {
       id: 1,
-      name: "kripik",
-      slug:  "stik-sd",
+      name: "kripik kentang",
+      slug:  "kripik-kentang",
       price: 15000,
-      image: "/assets/kripik/knt.png", // Pastikan jalur gambar mengarah ke public folder
-      link: "/produk/stik-kentang",
+      image: "/prod-k/prod-1.png", // Pastikan jalur gambar mengarah ke public folder
+      link: "/produk/kripik-kentang",
       details: [
         "Keripik kentang gurih, renyah, dan tahan lama.",
         "Dibuat dari bahan berkualitas pilihan.",
@@ -16,32 +16,107 @@ export const kripikProducts = [
     },
     {
       id: 2,
-      name: "Kripik dd",
-      slug: "stik-s",
+      name: "Kripik Pangsit",
+      slug: "kripik-pangsit",
       price: 15000,
-      image: "/assets/kripik/td.png",
-      link: "/produk/stik-bawang",
+      image: "/prod-k/prod-2.png",
+      link: "/produk/kripik-pangsit",
       details: [
-        "Keripik kentang gurih, renyah, dan tahan lama.",
-        "Dibuat dari bahan berkualitas pilihan.",
-        "Kemasan praktis dan higienis.",
-        "Cocok untuk camilan keluarga atau teman nongkrong.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
       ],
-      info: "Simpan di tempat sejuk dan kering agar tetap renyah. <br><br>Produk ini diproduksi oleh <strong>Cemil</strong> dan sudah memiliki izin P-IRT resmi. <br><br>Berat bersih: 100 gram / 200 gram.",
+      info: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Quisque quis metus a elit accumsan sodales. <br><br>Berat bersih: 100 gram / 200 gram."
     },
     {
       id: 3,
-      name: "Kripik Pangsit",
-      slug: "stik-fgg",
+      name: "Kripik Tempe",
+      slug: "kripik-tempe",
       price: 15000,
-      image: "/assets/kripik/kripik.png",
-      link: "/produk/stik-pangsit",
+      image: "/prod-k/prod-3.png",
+      link: "/produk/kripik-tempe",
       details: [
-        "Keripik kentang gurih, renyah, dan tahan lama.",
-        "Dibuat dari bahan berkualitas pilihan.",
-        "Kemasan praktis dan higienis.",
-        "Cocok untuk camilan keluarga atau teman nongkrong.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
       ],
-      info: "Simpan di tempat sejuk dan kering agar tetap renyah. <br><br>Produk ini diproduksi oleh <strong>Cemil</strong> dan sudah memiliki izin P-IRT resmi. <br><br>Berat bersih: 100 gram / 200 gram.",
-    }
+      info: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Quisque quis metus a elit accumsan sodales. <br><br>Berat bersih: 100 gram / 200 gram."
+    },
+    {
+      id: 4,
+      name: "Kripik Singkong",
+      slug: "kripik-singkong",
+      price: 15000,
+      image: "/prod-k/prod-4.png",
+      link: "/produk/kripik-singkong",
+      details: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      ],
+      info: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Quisque quis metus a elit accumsan sodales. <br><br>Berat bersih: 100 gram / 200 gram."
+    },
+    {
+      id: 5,
+      name: "Kripik Pisang",
+      slug: "kripik-pisang",
+      price: 15000,
+      image: "/prod-k/prod-5.png",
+      link: "/produk/kripik-tempe",
+      details: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      ],
+      info: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Quisque quis metus a elit accumsan sodales. <br><br>Berat bersih: 100 gram / 200 gram."
+    },
+    {
+      id: 6,
+      name: "Kripik Bayem",
+      slug: "kripik-bayem",
+      price: 15000,
+      image: "/prod-k/prod-6.png",
+      link: "/produk/kripik-bayem",
+      details: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      ],
+      info: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Quisque quis metus a elit accumsan sodales. <br><br>Berat bersih: 100 gram / 200 gram."
+    },
+    {
+      id: 7,
+      name: "Kripik Jamur",
+      slug: "kripik-jamur",
+      price: 15000,
+      image: "/prod-k/prod-7.png",
+      link: "/produk/kripik-jamur",
+      details: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      ],
+      info: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Quisque quis metus a elit accumsan sodales. <br><br>Berat bersih: 100 gram / 200 gram."
+    },
+    {
+      id: 8,
+      name: "Kripik Udang",
+      slug: "kripik-udang",
+      price: 15000,
+      image: "/prod-k/prod-8.png",
+      link: "/produk/kripik-udang",
+      details: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+      ],
+      info: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Quisque quis metus a elit accumsan sodales. <br><br>Berat bersih: 100 gram / 200 gram."
+    },
   ];
